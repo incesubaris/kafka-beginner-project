e87f86b5f1edda86074639b44312b6a60bf4b985
incesubaris <barisincesu@outlook.com>
2020-11-03 09:30:33 +0300

    init

---
+++ src/main/java/com/github/incesubaris/foo/producer.java
@@ -0,0 +1,65 @@
+package com.github.incesubaris.foo;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class producer{
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        final Logger logger = LoggerFactory.getLogger(producer.class);
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+
+        for (int i=0; i<10; i++ ) {
+            // create a producer record
+
+            String topic = "foo";
+            String value = "hello world " + Integer.toString(i);
+            String key = "id_" + Integer.toString(i);
+
+            ProducerRecord<String, String> record =
+                    new ProducerRecord<String, String>(topic, key, value);
+
+            logger.info("Key: " + key); // log the key
+
+            // send data - asynchronous
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    // executes every time a record is successfully sent or an exception is thrown
+                    if (e == null) {
+                        // the record was successfully sent
+                        logger.info("Received new metadata. \n");
+                        logger.info("Topic:" + recordMetadata.topic());
+                        logger.info("Partition: " + recordMetadata.partition());
+                        logger.info("Offset: " + recordMetadata.offset());
+                        logger.info("Timestamp: " + recordMetadata.timestamp());
+                    } else {
+                        logger.error("Error while producing", e);
+                    }
+                }
+            }).get(); // block the .send() to make it synchronous - don't do this in production!
+        }
+
+        // flush data
+        producer.flush();
+        // flush and close producer
+        producer.close();
+
+    }
+}
